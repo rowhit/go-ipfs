6a1f915915a961996c32f39d43580be7b20d1b8f
Łukasz Magiera <magik6k@gmail.com>
2018-07-19 13:18:05 +0200

    coreapi: dht: simplify the implementation
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- core/coreapi/dht.go
+++ core/coreapi/dht.go
@@ -8,89 +8,34 @@ import (
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
 	caopts "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
 	dag "github.com/ipfs/go-ipfs/merkledag"
+	"github.com/ipfs/go-ipfs/blockservice"
 
-	routing "gx/ipfs/QmZ383TySJVeZWzGnWui6pRcKyYZk9VkKTuW7tmKRWk5au/go-libp2p-routing"
-	notif "gx/ipfs/QmZ383TySJVeZWzGnWui6pRcKyYZk9VkKTuW7tmKRWk5au/go-libp2p-routing/notifications"
-	ipdht "gx/ipfs/QmQYwRL1T9dJtdCScoeRQwwvScbJTcWqnXhq4dYQ6Cu5vX/go-libp2p-kad-dht"
-	ma "gx/ipfs/QmYmsdtJ3HsodkePE3eU3TsCaP2YvPZJ4LoXnNkDE5Tpt7/go-multiaddr"
+	"gx/ipfs/QmZ383TySJVeZWzGnWui6pRcKyYZk9VkKTuW7tmKRWk5au/go-libp2p-routing"
 	pstore "gx/ipfs/QmZR2XWVVBCtbgBWnQhWk2xcQfaR3W8faQPriAiaaj7rsr/go-libp2p-peerstore"
-	peer "gx/ipfs/QmdVrMn1LhB4ybb8hMVaMLXnA8XRSewMnK6YqXKXoTcRvN/go-libp2p-peer"
-	cid "gx/ipfs/QmYVNvtQkeZ6AKSwDrjQTs432QtL6umrrK41EBq3cu7iSP/go-cid"
-	ipld "gx/ipfs/QmZtNq8dArGfnpCZfx2pUNY7UcjGhVp5qqwQ4hH6mpTMRQ/go-ipld-format"
+	"gx/ipfs/QmdVrMn1LhB4ybb8hMVaMLXnA8XRSewMnK6YqXKXoTcRvN/go-libp2p-peer"
+	"gx/ipfs/QmYVNvtQkeZ6AKSwDrjQTs432QtL6umrrK41EBq3cu7iSP/go-cid"
+	"gx/ipfs/QmRatnbGjPcoyzVjfixMZnuT1xQbjM7FgnL6FX4CKJeDE2/go-ipfs-blockstore"
+	"gx/ipfs/QmShbyKV9P7QuFecDHXsgrQ4rxxm71MUkGVpwedT4VQ8Bf/go-ipfs-exchange-offline"
 )
-
-var ErrNotDHT = errors.New("routing service is not a DHT")
 
 type DhtAPI struct {
 	*CoreAPI
 	*caopts.DhtOptions
 }
 
-func (api *DhtAPI) FindPeer(ctx context.Context, p peer.ID) (<-chan ma.Multiaddr, error) {
-	dht, ok := api.node.Routing.(*ipdht.IpfsDHT)
-	if !ok {
-		return nil, ErrNotDHT
+func (api *DhtAPI) FindPeer(ctx context.Context, p peer.ID) (pstore.PeerInfo, error) {
+	pi, err := api.node.Routing.FindPeer(ctx, peer.ID(p))
+	if err != nil {
+		return pstore.PeerInfo{}, err
 	}
 
-	outChan := make(chan ma.Multiaddr)
-	events := make(chan *notif.QueryEvent)
-	ctx = notif.RegisterForQueryEvents(ctx, events)
-
-	go func() {
-		defer close(outChan)
-
-		sendAddrs := func(responses []*pstore.PeerInfo) error {
-			for _, response := range responses {
-				for _, addr := range response.Addrs {
-					select {
-					case outChan <- addr:
-					case <-ctx.Done():
-						return ctx.Err()
-					}
-				}
-			}
-			return nil
-		}
-
-		for event := range events {
-			if event.Type == notif.FinalPeer {
-				err := sendAddrs(event.Responses)
-				if err != nil {
-					return
-				}
-			}
-		}
-	}()
-
-	go func() {
-		defer close(events)
-		pi, err := dht.FindPeer(ctx, peer.ID(p))
-		if err != nil {
-			notif.PublishQueryEvent(ctx, &notif.QueryEvent{
-				Type:  notif.QueryError,
-				Extra: err.Error(),
-			})
-			return
-		}
-
-		notif.PublishQueryEvent(ctx, &notif.QueryEvent{
-			Type:      notif.FinalPeer,
-			Responses: []*pstore.PeerInfo{&pi},
-		})
-	}()
-
-	return outChan, nil
+	return pi, nil
 }
 
-func (api *DhtAPI) FindProviders(ctx context.Context, p coreiface.Path, opts ...caopts.DhtFindProvidersOption) (<-chan peer.ID, error) {
+func (api *DhtAPI) FindProviders(ctx context.Context, p coreiface.Path, opts ...caopts.DhtFindProvidersOption) (<-chan pstore.PeerInfo, error) {
 	settings, err := caopts.DhtFindProvidersOptions(opts...)
 	if err != nil {
 		return nil, err
-	}
-
-	dht, ok := api.node.Routing.(*ipdht.IpfsDHT)
-	if !ok {
-		return nil, ErrNotDHT
 	}
 
 	rp, err := api.ResolvePath(ctx, p)
@@ -98,54 +43,13 @@ func (api *DhtAPI) FindProviders(ctx context.Context, p coreiface.Path, opts ...
 		return nil, err
 	}
 
-	c := rp.Cid()
-
 	numProviders := settings.NumProviders
 	if numProviders < 1 {
 		return nil, fmt.Errorf("number of providers must be greater than 0")
 	}
 
-	outChan := make(chan peer.ID)
-	events := make(chan *notif.QueryEvent)
-	ctx = notif.RegisterForQueryEvents(ctx, events)
-
-	pchan := dht.FindProvidersAsync(ctx, c, numProviders)
-	go func() {
-		defer close(outChan)
-
-		sendProviders := func(responses []*pstore.PeerInfo) error {
-			for _, response := range responses {
-				select {
-				case outChan <- response.ID:
-				case <-ctx.Done():
-					return ctx.Err()
-				}
-			}
-			return nil
-		}
-
-		for event := range events {
-			if event.Type == notif.Provider {
-				err := sendProviders(event.Responses)
-				if err != nil {
-					return
-				}
-			}
-		}
-	}()
-
-	go func() {
-		defer close(events)
-		for p := range pchan {
-			np := p
-			notif.PublishQueryEvent(ctx, &notif.QueryEvent{
-				Type:      notif.Provider,
-				Responses: []*pstore.PeerInfo{&np},
-			})
-		}
-	}()
-
-	return outChan, nil
+	pchan := api.node.Routing.FindProvidersAsync(ctx, rp.Cid(), numProviders)
+	return pchan, nil
 }
 
 func (api *DhtAPI) Provide(ctx context.Context, path coreiface.Path, opts ...caopts.DhtProvideOption) error {
@@ -156,10 +60,6 @@ func (api *DhtAPI) Provide(ctx context.Context, path coreiface.Path, opts ...cao
 
 	if api.node.Routing == nil {
 		return errors.New("cannot provide in offline mode")
-	}
-
-	if len(api.node.PeerHost.Network().Conns()) == 0 {
-		return errors.New("cannot provide, no connected peers")
 	}
 
 	rp, err := api.ResolvePath(ctx, path)
@@ -178,26 +78,8 @@ func (api *DhtAPI) Provide(ctx context.Context, path coreiface.Path, opts ...cao
 		return fmt.Errorf("block %s not found locally, cannot provide", c)
 	}
 
-	//TODO: either remove or use
-	//outChan := make(chan interface{})
-
-	//events := make(chan *notif.QueryEvent)
-	//ctx = notif.RegisterForQueryEvents(ctx, events)
-
-	/*go func() {
-		defer close(outChan)
-		for range events {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-			}
-		}
-	}()*/
-
-	//defer close(events)
 	if settings.Recursive {
-		err = provideKeysRec(ctx, api.node.Routing, api.node.DAG, []*cid.Cid{c})
+		err = provideKeysRec(ctx, api.node.Routing, api.node.Blockstore, []*cid.Cid{c})
 	} else {
 		err = provideKeys(ctx, api.node.Routing, []*cid.Cid{c})
 	}
@@ -218,10 +100,12 @@ func provideKeys(ctx context.Context, r routing.IpfsRouting, cids []*cid.Cid) er
 	return nil
 }
 
-func provideKeysRec(ctx context.Context, r routing.IpfsRouting, dserv ipld.DAGService, cids []*cid.Cid) error {
+func provideKeysRec(ctx context.Context, r routing.IpfsRouting, bs blockstore.Blockstore, cids []*cid.Cid) error {
 	provided := cid.NewSet()
 	for _, c := range cids {
 		kset := cid.NewSet()
+
+		dserv := dag.NewDAGService(blockservice.New(bs, offline.Exchange(bs)))
 
 		err := dag.EnumerateChildrenAsync(ctx, dag.GetLinksDirect(dserv), c, kset.Visit)
 		if err != nil {
--- core/coreapi/interface/dht.go
+++ core/coreapi/interface/dht.go
@@ -3,21 +3,21 @@ package iface
 import (
 	"context"
 
-	options "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
+	"github.com/ipfs/go-ipfs/core/coreapi/interface/options"
 
-	ma "gx/ipfs/QmYmsdtJ3HsodkePE3eU3TsCaP2YvPZJ4LoXnNkDE5Tpt7/go-multiaddr"
-	peer "gx/ipfs/QmdVrMn1LhB4ybb8hMVaMLXnA8XRSewMnK6YqXKXoTcRvN/go-libp2p-peer"
+	"gx/ipfs/QmdVrMn1LhB4ybb8hMVaMLXnA8XRSewMnK6YqXKXoTcRvN/go-libp2p-peer"
+	pstore "gx/ipfs/QmZR2XWVVBCtbgBWnQhWk2xcQfaR3W8faQPriAiaaj7rsr/go-libp2p-peerstore"
 )
 
 // DhtAPI specifies the interface to the DHT
 type DhtAPI interface {
 	// FindPeer queries the DHT for all of the multiaddresses associated with a
 	// Peer ID
-	FindPeer(context.Context, peer.ID) (<-chan ma.Multiaddr, error)
+	FindPeer(context.Context, peer.ID) (pstore.PeerInfo, error)
 
 	// FindProviders finds peers in the DHT who can provide a specific value
 	// given a key.
-	FindProviders(context.Context, Path, ...options.DhtFindProvidersOption) (<-chan peer.ID, error) //TODO: is path the right choice here?
+	FindProviders(context.Context, Path, ...options.DhtFindProvidersOption) (<-chan pstore.PeerInfo, error)
 
 	// WithNumProviders is an option for FindProviders which specifies the
 	// number of peers to look for. Default is 20
